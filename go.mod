18ad8a0d5636bc2bc1c112370a8ffc6ee1a3372f
snowmerak <snowmerak@outlook.com>
2025-12-16 01:31:41 +0900

    feat(encryption): add XChaCha20-Poly1305 encryptor implementation

--- go.mod
+++ go.mod
@@ -5,6 +5,8 @@ go 1.25.5
 require github.com/google/go-tpm v0.9.7
 
 require (
+	github.com/awnumar/memcall v0.4.0 // indirect
+	github.com/awnumar/memguard v0.23.0 // indirect
 	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgraph-io/badger/v4 v4.8.0 // indirect
 	github.com/dgraph-io/ristretto/v2 v2.2.0 // indirect
@@ -17,7 +19,8 @@ require (
 	go.opentelemetry.io/otel v1.37.0 // indirect
 	go.opentelemetry.io/otel/metric v1.37.0 // indirect
 	go.opentelemetry.io/otel/trace v1.37.0 // indirect
-	golang.org/x/net v0.41.0 // indirect
-	golang.org/x/sys v0.34.0 // indirect
+	golang.org/x/crypto v0.46.0 // indirect
+	golang.org/x/net v0.47.0 // indirect
+	golang.org/x/sys v0.39.0 // indirect
 	google.golang.org/protobuf v1.36.6 // indirect
 )
---
+++ xchacha20_poly1305.go
@@ -0,0 +1,73 @@
+package tpmops
+
+import (
+	"crypto/rand"
+	"fmt"
+
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+type Encryptor interface {
+	Encrypt(plaintext []byte) (ciphertext []byte, err error)
+	Decrypt(ciphertext []byte) (plaintext []byte, err error)
+}
+
+type XChaCha20Poly1305Encryptor struct {
+	encryptedKey []byte
+	loadedKey    *LoadedKey
+}
+
+func NewXChaCha20Poly1305Encryptor(encryptedKey []byte, loadedKey *LoadedKey) *XChaCha20Poly1305Encryptor {
+	return &XChaCha20Poly1305Encryptor{
+		encryptedKey: encryptedKey,
+		loadedKey:    loadedKey,
+	}
+}
+
+func (e *XChaCha20Poly1305Encryptor) Encrypt(plaintext []byte) ([]byte, error) {
+	k, err := e.loadedKey.Decrypt(e.encryptedKey)
+	if err != nil {
+		return nil, fmt.Errorf("decrypting XChaCha20-Poly1305 key: %w", err)
+	}
+
+	aead, err := chacha20poly1305.NewX(k)
+	if err != nil {
+		return nil, fmt.Errorf("creating XChaCha20-Poly1305 cipher: %w", err)
+	}
+
+	nonce := make([]byte, chacha20poly1305.NonceSizeX)
+	if _, err := rand.Read(nonce); err != nil {
+		return nil, fmt.Errorf("generating nonce: %w", err)
+	}
+
+	ciphertext := aead.Seal(nil, nonce, plaintext, nil)
+	ciphertext = append(nonce, ciphertext...)
+
+	return ciphertext, nil
+}
+
+func (e *XChaCha20Poly1305Encryptor) Decrypt(ciphertext []byte) ([]byte, error) {
+	k, err := e.loadedKey.Decrypt(e.encryptedKey)
+	if err != nil {
+		return nil, fmt.Errorf("decrypting XChaCha20-Poly1305 key: %w", err)
+	}
+
+	aead, err := chacha20poly1305.NewX(k)
+	if err != nil {
+		return nil, fmt.Errorf("creating XChaCha20-Poly1305 cipher: %w", err)
+	}
+
+	if len(ciphertext) < chacha20poly1305.NonceSizeX {
+		return nil, fmt.Errorf("ciphertext too short")
+	}
+
+	nonce := ciphertext[:chacha20poly1305.NonceSizeX]
+	ciphertextData := ciphertext[chacha20poly1305.NonceSizeX:]
+
+	plaintext, err := aead.Open(nil, nonce, ciphertextData, nil)
+	if err != nil {
+		return nil, fmt.Errorf("decrypting data: %w", err)
+	}
+
+	return plaintext, nil
+}
